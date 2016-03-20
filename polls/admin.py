8ca9477a63d2af9e6d9763444544fc575d9b864c
Benjamin Yan <ycs_ctbu_2010@126.com>
2016-03-20 23:43:04 +0800

    admin coding

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from polls.models import Poll, Choice
+
+
 # Register your models here.
+class ChoiceAdmin(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question"]
+    fieldsets = [
+        (None, {'fields': ["question"]}),
+        ("Date information", {'fields': ["pub_date"], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceAdmin]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
+
 import datetime
 
-from django.utils import timezone
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -12,10 +13,16 @@ class Poll(models.Model):
     pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
-        self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __unicode__(self):
         return self.question
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently'
+
+
 
 
 class Choice(models.Model):
